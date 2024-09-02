d59c84a0ed39d85a78d5f065ea085786d2bb1617
bimasuluk <akunpelajaran49@gmail.com>
2024-09-02 13:08:39 +0700

    suluk

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const cors = require('cors'); // Import cors
 const app = express()
 const port = 3005
 
 //rute data
 const masterAdmin = require('./routes/AbsensiSiswa/dashboar/user')
 const LoginDash = require('./routes/AbsensiSiswa/dashboar/login')
+
+// Gunakan cors dengan konfigurasi untuk mengizinkan permintaan dari http://localhost:3000
+app.use(cors({
+    origin: 'http://localhost:3000'
+  }));
 
  // Middleware untuk mengurai body dalam format JSON
 app.use(express.json())
