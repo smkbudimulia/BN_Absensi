2a0d8c2d44cb79f69c9026ff58e839de92da6dbf
lukimme <lukmanhakim8832@gmail.com>
2024-10-07 08:53:40 +0700

    TA

--- routes/masterData/tahunAjaran.js
+++ routes/masterData/tahunAjaran.js
@@ -22,8 +22,8 @@ function generateRandomString(length) {
   }
 
   // Operasi Post: luntuk menambah data baru
-router.post('/new-tahun-ajaran', async (req, res) =>{
-    const {id_tahun_pelajaran, id_admin, tahun, aktif} =req.body
+router.post('/new-tahun-pelajaran', async (req, res) =>{
+    const { id_tahun_pelajaran, id_admin, tahun, aktif } =req.body
     const idAcak = generateRandomString(5);
 
     //validai input data
@@ -62,10 +62,128 @@ router.post('/new-tahun-ajaran', async (req, res) =>{
         })
 
     } catch (error) {
-        
-        
+        console.log(error);
+        res.status(500).json({ 
+          Status: 500,
+          error: 'Internal Server Error' 
+        });        
+    }
+})
+
+//operasi read: melihat semua data
+router.get('/all-tahun-pelajaran', (req, res)=>{
+    try {
+        conn('tahun_ajaran')
+        .select('*')
+        .then((data)=>{
+            res.status(200).json({
+                Status: 200,
+                message: "ok",
+                data: data
+            })
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ 
+          Status: 500,
+          error: 'Internal Server Error' 
+        });        
+    }
+   
+})
+
+// Operasi Put/ Update: merubah data yang sudah ada pada database
+router.put('/Edit-tahun-pelajaran/:id', async (req, res) =>{
+    const id_tahun_pelajaran = req.params.id;
+    const {  id_admin, tahun, aktif } = req.body
+
+    //validasi inputan kosong
+    if (!tahun || !aktif) {
+        return res.status(400).json({
+            Status: 400,
+            error: 'Data tidak boleh kosong'
+        })
     }
 
+    try {
+        //cek apakah data dengan ID yg dimaksud ada
+        const existingTahunAjaran = await conn('tahun_ajaran')
+        .where('id_tahun_pelajaran', id_tahun_pelajaran)
+        .first()
+
+        if (!existingTahunAjaran) {
+            return res.status(404).json({
+                Status: 404,
+                error: 'Tidak ada data'
+            })            
+        }
+        const duplicateCheck = await conn('tahun_ajaran')
+        .where(function(){
+            this.where('tahun', tahun)
+        })
+        if (duplicateCheck) {
+            return res.status(400).json({
+                Status: 400,
+                error: 'Tahun sudah ada'
+            })
+        }
+
+        //update data
+        const updateTahunPelajaran = {
+            tahun,
+            aktif
+        }
+
+        await conn('tahun_ajaran')
+        .where('id_tahun_pelajaran', id_tahun_pelajaran)
+        .update(updateTahunPelajaran)
+
+        res.status(200).json({
+            Status: 200,
+            message: 'Data berhasil diperbarui',
+            data: updateTahunPelajaran
+        })
+    } catch (error) {
+        console.error(error);
+    res.status(500).json({
+      Status: 500,
+      error: 'Internal Server Error'
+    })
+    }
+})
+
+//operasi delete: menghapus data by Id
+router.delete('/hapus-tahun-pelajaran/:id', async (req, res)=>{
+    const id_tahun_pelajaran = req.params.id;
+
+    try {
+        //cek apakah Id yang dimaksud ada.!
+        const existingTahunAjaran = await conn('tahun_ajaran')
+        .where('id_tahun_pelajaran', id_tahun_pelajaran)
+        .first()
+
+        if (!existingTahunAjaran) {
+            return res.status(404).json({
+                Status: 404,
+                error: 'Tidak ada data'
+            })            
+        }
+        // hapus tahun pelajaran berdasarkan id
+        await conn('tahun_ajaran')
+        .where('id_tahun_pelajaran', id_tahun_pelajaran)
+        .del();
+
+        res.status(200).json({
+            Status: 200,
+            message: 'Data berhasil dihapus'
+        })
+    } catch (error) {
+        console.error(error);
+    res.status(500).json({
+      Status: 500,
+      error: 'Internal Server Error'
+    })        
+    }
 })
 
 module.exports = router;
