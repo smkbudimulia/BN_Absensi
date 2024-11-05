c713bb3c0b6004d050b48f2051fa4d4cb7ac0dda
lukimme <lukmanhakim8832@gmail.com>
2024-11-05 12:02:40 +0700

    oko

--- routes/NonMasterData/joinData.js
+++ routes/NonMasterData/joinData.js
@@ -5,26 +5,36 @@ const conn = require('../../Database/ConfigDB')// koneksi ke database
 const verifyToken = require('../../middleware/jwToken')
 
 // data jumlah kelas dan siswanya
-router.get('/KelasSiswaTotal', async(req, res)=>{
- try {
-        conn('siswa')
-        .select('id_kelas','id_rombel')
-        .count('* as total')
-        .groupBy('id_kelas','id_rombel')
-        .then((data)=>{
+router.get('/KelasSiswaTotal', async (req, res) => {
+    try {
+        const data = await conn('siswa')
+            .select('id_kelas', 'id_rombel')
+            .count('* as total')
+            .groupBy('id_kelas', 'id_rombel');
+
+        if (data.length === 0) {
+            // Jika tidak ada data, kirimkan respons kosong
+            res.status(200).json({
+                Status: 200,
+                message: "No data found",
+                data: []
+            });
+        } else {
+            // Jika ada data, kirimkan respons dengan hasil
             res.status(200).json({
                 Status: 200,
                 message: "ok",
                 data: data
-            })
-        })
+            });
+        }
     } catch (error) {
         console.log(error);
-        res.status(500).json({ 
-          Status: 500,
-          error: 'Internal Server Error' 
-        });        
+        res.status(500).json({
+            Status: 500,
+            error: 'Internal Server Error'
+        });
     }
-})
+});
+
 
 module.exports = router;
